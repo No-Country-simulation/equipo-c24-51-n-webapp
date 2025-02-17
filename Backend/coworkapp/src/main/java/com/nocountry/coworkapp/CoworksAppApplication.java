ae19f09456a1b79a71f71cf4cbf9892bc661607c
ManuMarcos <manuelignaciomarcos@hotmail.com>
2025-02-17 12:35:11 -0300

    First commit: adding project structure

---
+++ Backend/coworkapp/src/main/java/com/nocountry/coworkapp/CoworksAppApplication.java
@@ -0,0 +1,13 @@
+package com.nocountry.coworkapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoworksAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoworksAppApplication.class, args);
+	}
+
+}
---
+++ Backend/coworkapp/src/test/java/com/nocountry/coworkapp/CoworksAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nocountry.coworkapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoworksAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
